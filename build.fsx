40aecab9ed9604744dee94aeb63b22cd9830671a
Thomas Monte <thomasmont3@gmail.com>
2018-07-27 19:01:24 -0600

    agora vai (1)

--- build.fsx
+++ build.fsx
@@ -1,22 +1,49 @@
 #load ".fake/build.fsx/intellisense.fsx"
+
+
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
-open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
-let testDir  = "./tests/"
+let run (cmd:string) dir args  =
+    if Process.execSimple (fun info ->
+        { info with
+            FileName = cmd
+            WorkingDirectory =
+                if not (String.isNullOrWhiteSpace dir) then dir else info.WorkingDirectory
+            Arguments = args
+        }
+    ) System.TimeSpan.MaxValue <> 0 then
+        failwithf "Error while running '%s' with args: %s " cmd args
+
+let npm = run "npm" "./"
 
 Target.create "Clean" (fun _ ->
     !! "src/**/bin"
     ++ "src/**/obj"
+    ++ "docs/**/bin"
+    ++ "docs/**/obj"
+    ++ "tests/**/bin"
+    ++ "tests/**/obj"
+    ++ "build"
     |> Shell.cleanDirs 
 )
 
 Target.create "Build" (fun _ ->
     !! "src/**/*.*proj"
     |> Seq.iter (DotNet.build id)
+)
+
+Target.create "BuildDocs" (fun _ ->
+    !! "docs/**/*.*.proj"
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "NPMBuildDocs" (fun _ ->
+    npm "install"
+    npm "run build"
 )
 
 Target.create "BuildTests" (fun _ ->
@@ -34,6 +61,8 @@ Target.create "All" ignore
 
 "Clean"
   ==> "Build"
+  ==> "BuildDocs"
+  ==> "NPMBuildDocs"
   ==> "BuildTests"
   ==> "Test"
   ==> "All"
