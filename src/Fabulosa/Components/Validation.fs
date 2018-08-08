0c51b78a39ad003f8cf241e8206e857acd3b66eb
Thomas Monte <thomasmont3@gmail.com>
2018-08-08 14:55:35 -0600

    validation

---
+++ src/Fabulosa/Components/Validation.fs
@@ -0,0 +1,30 @@
+namespace Fabulosa
+
+[<RequireQualifiedAccess>]
+module Validation =
+
+    module R = Fable.Helpers.React
+    open R.Props
+
+    [<RequireQualifiedAccess>]
+    type Kind =
+    | Error of string
+    | Success of string
+
+    let message =
+        function
+        | Kind.Success message -> message 
+        | Kind.Error message -> message
+
+    let className =
+        function
+        | Kind.Success _ -> "has-success"
+        | Kind.Error _ -> "has-error"
+
+    let test = R.div [] [R.str "WHATEVA"]
+
+    let ƒ (kind: Kind) elem =
+        R.span [ClassName <| className kind] [
+            elem
+            R.p [ClassName "form-input-hint"] [R.str <| message kind]
+        ]
