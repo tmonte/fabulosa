f2705c0e0c66ff4fa9bf4d8ae5c0c0ed9da663f6
Thomas Monte <thomasmont3@gmail.com>
2018-08-07 20:25:46 -0600

    first take at input group

--- src/Fabulosa/Components/Input.fs
+++ src/Fabulosa/Components/Input.fs
@@ -41,8 +41,6 @@ module Input =
 [<RequireQualifiedAccess>]
 module IconInput =
 
-    open ReactAPIExtensions
-    open Fable.Import.React
     module R = Fable.Helpers.React
     open R.Props
 
@@ -54,10 +52,14 @@ module IconInput =
     [<RequireQualifiedAccess>]
     type Props = {
         Position: Position
+        InputProps: Input.Props
+        IconProps: Icon.Props
     }
 
     let defaults = {
         Props.Position = Position.Left
+        Props.InputProps = Input.defaults
+        Props.IconProps = Icon.defaults
     }
 
     let position =
@@ -65,11 +67,78 @@ module IconInput =
         | Position.Left -> "has-icon-left"
         | Position.Right -> "has-icon-right"
 
-    let ƒ (props: Props) (children: ReactElement list) =
+    let ƒ (props: Props) =
         let containerClasses =
             [ position props.Position ]
             |> String.concat " "
+        let iconProps =
+            { props.IconProps with
+                HTMLProps = props.IconProps.HTMLProps
+                @ [ClassName "form-icon"] }
         R.div [ClassName containerClasses] [
-            children.[0]
-            children.[1] |> appendClass ["form-icon"]
+            Input.ƒ props.InputProps
+            Icon.ƒ iconProps []
+        ]
+
+[<RequireQualifiedAccess>]
+module InputGroup =
+
+    open Fable.Import.React
+    module R = Fable.Helpers.React
+    open R.Props
+
+    type AddOn =
+    | Radio of Radio.Props
+    | Checkbox of Checkbox.Props
+    | Switch of Switch.Props
+    | Unset
+
+    [<RequireQualifiedAccess>]
+    type WithButton =
+    | Button of Button.Props
+    | Unset
+
+    type WithText =
+    | Text of string
+    | Unset
+
+    [<RequireQualifiedAccess>]
+    type Props = {
+        AddonProps: AddOn
+        WithButton: WithButton
+        WithText: WithText
+        HTMLProps: IHTMLProp list
+    }
+
+    let defaults = {
+        Props.AddonProps = AddOn.Unset
+        Props.WithButton = WithButton.Unset
+        Props.WithText = WithText.Unset
+        Props.HTMLProps = []
+    }
+
+    let button =
+        function
+        | WithButton.Button props ->
+            Some <| Button.ƒ
+                { props with
+                    HTMLProps = props.HTMLProps
+                    @ [ClassName "input-group-btn"] } []
+        | WithButton.Unset -> None
+
+    let text =
+        function
+        | WithText.Text text ->
+            Some <| R.span [ClassName "input-group-addon"] [R.str text]
+        | WithText.Unset -> None
+
+    let empty = R.str ""
+
+    let ƒ (props: Props) (children: ReactElement list) =
+        let button = button props.WithButton
+        let text = text props.WithText
+        R.div [ClassName "input-group"] [
+            (if text.IsSome then text.Value else empty)
+            R.fragment [] children
+            (if button.IsSome then button.Value else empty)
         ]
