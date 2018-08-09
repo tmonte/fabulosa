776b436308bc6325fd4d2ae38d3a561d99870213
Thomas Monte <thomasmont3@gmail.com>
2018-08-09 15:20:55 -0600

    more docs on forms

--- docs/Fabulosa.Docs/Pages/FormPage.fs
+++ docs/Fabulosa.Docs/Pages/FormPage.fs
@@ -5,34 +5,94 @@ open R.Props
 open Fabulosa
 open Fabulosa.Docs.JavascriptMapping
 
-let inputSnippet = """(* Form Group *)
-Form.Group.ƒ Form.Group.defaults [
+let inputSnippet = """(* First Name Input *)
+Label.ƒ { Label.defaults with Text = "First Name" }
+Input.ƒ {
+    Input.defaults with
+        HTMLProps = [Placeholder "Please enter your first name"]
+}
 
-    (* First Name Input *)
-    Label.ƒ { Label.defaults with Text = "First Name" }
-    Input.ƒ {
-        Input.defaults with
-            HTMLProps = [Placeholder "Please enter your first name"]
-    }
+(* Time Input With Icon *)
+Label.ƒ { Label.defaults with Text = "Time" }
+IconInput.ƒ {
+    IconInput.defaults with
+        InputProps =
+            { Input.defaults with
+                HTMLProps = [Placeholder "Please enter a time"] }
+        IconProps = { Icon.defaults with Kind = Icon.Kind.Time }
+}
+"""
 
-    (* Last Name Input *)
-    Label.ƒ { Label.defaults with Text = "Last Name" }
-    Input.ƒ {
-        Input.defaults with
-            HTMLProps = [Placeholder "Please enter your last name"]
-    }
+let textareaSnippet = """(* Description Textarea *)
+Label.ƒ { Label.defaults with Text = "Description" }
+Textarea.ƒ {
+    Textarea.defaults with
+        HTMLProps = [Placeholder "Please enter a description"]
+}
+"""
+
+let selectSnippet = """(* Choose Animal Select *)
+Label.ƒ { Label.defaults with Text = "Choose Animal" }
+Select.ƒ Select.defaults [
+    Select.Option.ƒ Select.Option.defaults [R.str "Dog"]
+    Select.Option.ƒ Select.Option.defaults [R.str "Cat"]
+    Select.Option.ƒ Select.Option.defaults [R.str "Parrot"]
 ]
 """
 
-let selectSnippet = """(* Form Group *)
-Form.Group.ƒ Form.Group.defaults [
+let crsSnippet = """(* Contact Preferences Radio *)
+Label.ƒ { Label.defaults with Text = "Contact preferences" }
+Radio.ƒ {
+    Radio.defaults with
+        HTMLProps = [Name "contact-prefs"]
+        Text = "Call me"
+}
+Radio.ƒ {
+    Radio.defaults with
+        HTMLProps = [Name "contact-prefs"]
+        Text = "Text me"
+}
 
-    (* Choose Animal Select *)
-    Label.ƒ { Label.defaults with Text = "Choose Animal" }
+(* Login Options Checkbox *)
+Label.ƒ { Label.defaults with Text = "Login Options" }
+Checkbox.ƒ { Checkbox.defaults with Text = "Remember me" }
+
+(* Profile Preferences Switch*)
+Label.ƒ { Label.defaults with Text = "Profile preferences" }
+Switch.ƒ { Switch.defaults with Text = "Link my github account" }
+"""
+
+let inputGroupSnippet = """(* Website Input Group *)
+Label.ƒ { Label.defaults with Text = "Email Address" }
+InputGroup.ƒ {
+    InputGroup.defaults with
+        AddonLeft = InputGroup.AddonLeft.Text "https://"                                        
+        AddonRight = InputGroup.AddonRight.Button
+            ( { Button.defaults with Kind = Button.Kind.Primary },
+            [R.str "Validate"] )
+} [
+    Input.ƒ {
+        Input.defaults with
+            HTMLProps = [Placeholder "Please enter website address"]
+    }
+]
+
+(* Search Engine Input Group *)
+Label.ƒ { Label.defaults with Text = "Search Engine" }
+InputGroup.ƒ {
+    InputGroup.defaults with
+        AddonLeft = InputGroup.AddonLeft.Text "https://"                                        
+        AddonRight = InputGroup.AddonRight.Button
+            ( Button.defaults, [
+                Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Search } []
+                R.RawText "\n"
+                R.str "Search"
+            ] )
+} [
     Select.ƒ Select.defaults [
-        Select.Option.ƒ Select.Option.defaults [R.str "Dog"]
-        Select.Option.ƒ Select.Option.defaults [R.str "Cat"]
-        Select.Option.ƒ Select.Option.defaults [R.str "Parrot"]
+        Select.Option.ƒ Select.Option.defaults [R.str "google.com"]
+        Select.Option.ƒ Select.Option.defaults [R.str "yahoo.com"]
+        Select.Option.ƒ Select.Option.defaults [R.str "duckduckgo.com"]
     ]
 ]
 """
@@ -57,10 +117,13 @@ let view () =
                         Input.defaults with
                             HTMLProps = [Placeholder "Please enter your first name"]
                     }
-                    Label.ƒ { Label.defaults with Text = "Last Name" }
-                    Input.ƒ {
-                        Input.defaults with
-                            HTMLProps = [Placeholder "Please enter your last name"]
+                    Label.ƒ { Label.defaults with Text = "Time" }
+                    IconInput.ƒ {
+                        IconInput.defaults with
+                            InputProps =
+                                { Input.defaults with
+                                    HTMLProps = [Placeholder "Please enter a time"] }
+                            IconProps = { Icon.defaults with Kind = Icon.Kind.Time }
                     }
                 ]
             ]
@@ -70,6 +133,29 @@ let view () =
                 Code.ƒ {
                     Code.defaults with 
                         Code = Highlight.fsharp inputSnippet
+                }
+            ]
+        ]
+        R.h4 [ClassName "s-title"]
+            [R.str "Textarea"]
+        Grid.Row.ƒ {
+            Grid.Row.defaults with HTMLProps = [ClassName "docs-demo"]
+        } [
+            Grid.Column.ƒ { Grid.Column.defaults with Size = 12 } [
+                Form.Group.ƒ Form.Group.defaults [
+                    Label.ƒ { Label.defaults with Text = "Description" }
+                    Textarea.ƒ {
+                        Textarea.defaults with
+                            HTMLProps = [Placeholder "Please enter a description"]
+                    } []
+                ]
+            ]
+        ]
+        Grid.Row.ƒ Grid.Row.defaults [
+            Grid.Column.ƒ { Grid.Column.defaults with Size = 12 } [
+                Code.ƒ {
+                    Code.defaults with 
+                        Code = Highlight.fsharp textareaSnippet
                 }
             ]
         ]
@@ -96,55 +182,83 @@ let view () =
                 }
             ]
         ]
-        Form.Group.ƒ Form.Group.defaults [
-            Label.ƒ { Label.defaults with Text = "Time" }
-            IconInput.ƒ
-                { IconInput.defaults with
-                    InputProps =
-                        { Input.defaults with
-                            HTMLProps = [Placeholder "Please enter something"] };
-                    IconProps = { Icon.defaults with Kind = Icon.Kind.Time } }
-        ]
-        Form.Group.ƒ Form.Group.defaults [
-            Label.ƒ { Label.defaults with Text = "Profile description" }
-            Textarea.ƒ
-                { Textarea.defaults with
-                    HTMLProps = [Placeholder "Please enter a description"] } []
-        ]
-        Form.Group.ƒ Form.Group.defaults [
-            Label.ƒ { Label.defaults with Text = "Contact preferences" }
-            Radio.ƒ
-                { Radio.defaults with
-                    HTMLProps = [Name "contact-prefs"];
-                    Text = "Call me" }
-                
-            Radio.ƒ
-                { Radio.defaults with
-                    HTMLProps = [Name "contact-prefs"];
-                    Text = "Text me" }
-        ]
-        Form.Group.ƒ Form.Group.defaults [
-            Label.ƒ { Label.defaults with Text = "Profile preferences" }
-            Switch.ƒ { Switch.defaults with Text = "Link my github account" }
-        ]
-        Form.Group.ƒ Form.Group.defaults [
-            Checkbox.ƒ { Checkbox.defaults with Text = "Remember me" }
-            Checkbox.ƒ { Checkbox.defaults with Text = "Forget me" }
-        ]
-        Form.Group.ƒ Form.Group.defaults [
-            InputGroup.ƒ
-                { InputGroup.defaults with
-                    AddonLeft = InputGroup.AddonLeft.Text "someprefix/"                                        
-                    AddonRight = InputGroup.AddonRight.Button
-                        ( { Button.defaults with Kind = Button.Kind.Primary },
-                        [R.str "Save"] )
-                } [
-                    Select.ƒ Select.defaults [
-                        Select.Option.ƒ Select.Option.defaults [R.str "Option 1"]
-                        Select.Option.ƒ Select.Option.defaults [R.str "Option 2"]
-                    ]
-                    Input.ƒ { Input.defaults with HTMLProps = [Placeholder "Enter text"] }
+        R.h4 [ClassName "s-title"] [R.str "Radio, Checkbox and Switch"]
+        Grid.Row.ƒ {
+            Grid.Row.defaults with HTMLProps = [ClassName "docs-demo"]
+        } [
+            Grid.Column.ƒ { Grid.Column.defaults with Size = 12 } [
+                Form.Group.ƒ Form.Group.defaults [
+                    Label.ƒ { Label.defaults with Text = "Contact preferences" }
+                    Radio.ƒ {
+                        Radio.defaults with
+                            HTMLProps = [Name "contact-prefs"]
+                            Text = "Call me"
+                    }
+                    Radio.ƒ {
+                        Radio.defaults with
+                            HTMLProps = [Name "contact-prefs"]
+                            Text = "Text me"
+                    }
+                    Label.ƒ { Label.defaults with Text = "Login Options" }
+                    Checkbox.ƒ { Checkbox.defaults with Text = "Remember me" }
+                    Label.ƒ { Label.defaults with Text = "Profile preferences" }
+                    Switch.ƒ { Switch.defaults with Text = "Link my github account" }
                 ]
+            ]
         ]
-        Anchor.ƒ Anchor.defaults [R.str "Submit"]
+        Grid.Row.ƒ Grid.Row.defaults [
+            Grid.Column.ƒ { Grid.Column.defaults with Size = 12 } [
+                Code.ƒ {
+                    Code.defaults with 
+                        Code = Highlight.fsharp crsSnippet
+                }
+            ]
+        ]
+        R.h4 [ClassName "s-title"] [R.str "Input Group"]
+        Grid.Row.ƒ {
+            Grid.Row.defaults with HTMLProps = [ClassName "docs-demo"]
+        } [
+            Grid.Column.ƒ { Grid.Column.defaults with Size = 12 } [
+                Form.Group.ƒ Form.Group.defaults [
+                    Label.ƒ { Label.defaults with Text = "Website" }
+                    InputGroup.ƒ {
+                        InputGroup.defaults with
+                            AddonLeft = InputGroup.AddonLeft.Text "https://"                                        
+                            AddonRight = InputGroup.AddonRight.Button
+                                ( { Button.defaults with Kind = Button.Kind.Primary },
+                                [R.str "Validate"] )
+                    } [
+                        Input.ƒ {
+                            Input.defaults with
+                                HTMLProps = [Placeholder "Please enter website address"]
+                        }
+                    ]
+                    Label.ƒ { Label.defaults with Text = "Search Engine" }
+                    InputGroup.ƒ {
+                        InputGroup.defaults with
+                            AddonLeft = InputGroup.AddonLeft.Text "https://"                                        
+                            AddonRight = InputGroup.AddonRight.Button
+                                ( Button.defaults, [
+                                    Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Search } []
+                                    R.RawText "\n"
+                                    R.str "Search"
+                                ] )
+                    } [
+                        Select.ƒ Select.defaults [
+                            Select.Option.ƒ Select.Option.defaults [R.str "google.com"]
+                            Select.Option.ƒ Select.Option.defaults [R.str "yahoo.com"]
+                            Select.Option.ƒ Select.Option.defaults [R.str "duckduckgo.com"]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+        Grid.Row.ƒ Grid.Row.defaults [
+            Grid.Column.ƒ { Grid.Column.defaults with Size = 12 } [
+                Code.ƒ {
+                    Code.defaults with 
+                        Code = Highlight.fsharp inputGroupSnippet
+                }
+            ]
+        ]
     ]
