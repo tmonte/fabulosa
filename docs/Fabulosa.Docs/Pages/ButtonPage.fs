1e57a6011963b070f74a49c501589eb48a22a31d
Thomas Monte <thomasmont3@gmail.com>
2018-08-09 09:41:08 -0600

    added code to button page

--- docs/Fabulosa.Docs/Pages/ButtonPage.fs
+++ docs/Fabulosa.Docs/Pages/ButtonPage.fs
@@ -2,9 +2,107 @@ module ButtonPage
 
 module R = Fable.Helpers.React
 open R.Props
-open Fable.Import.Browser
 open Fabulosa
-open Fabulosa.Index
+open Fabulosa.Docs.JavascriptMapping
+
+let buttonKindSnippet = """(* Default Button *)
+Button.ƒ
+    Button.defaults
+    [R.str "Default"]
+
+(* Primary Button *)
+Button.ƒ
+    { Button.defaults with
+        Kind = Button.Kind.Primary }
+    [R.str "Primary"]
+
+(* Link Button *)
+Button.ƒ
+    { Button.defaults with
+        Kind = Button.Kind.Link }
+    [R.str "Link"]
+"""
+
+
+let buttonSizeSnippet = """(* Small Button *)
+Button.ƒ
+    { Button.defaults with
+        Size = Button.Size.Small }    
+    [R.str "Small"]
+
+(* Medium Button *)
+Button.ƒ
+    Button.defaults
+    [R.str "Default"]
+
+(* Large Button *)
+Button.ƒ
+    { Button.defaults with
+        Size = Button.Size.Large }
+    [R.str "Large"]
+"""
+
+let buttonColorSnippet = """(* Success Button *)
+Button.ƒ
+    { Button.defaults with
+        Color = Button.Color.Success }
+    [R.str "Success"]
+
+(* Error Button *)
+Button.ƒ
+    { Button.defaults with
+        Color = Button.Color.Error }
+    [R.str "Error"]
+"""
+
+let buttonFormatSnippet = """(* Squared Action Button *)
+Button.ƒ
+    { Button.defaults with
+        Format = Button.Format.SquaredAction
+        Kind = Button.Kind.Primary }
+    [ Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Plus } [] ]
+
+(* Round Action Button *)
+Button.ƒ
+    { Button.defaults with
+        Format = Button.Format.RoundAction }
+    [ Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Plus } [] ]
+"""
+
+let buttonStateSnippet = """(* Disabled Button *)
+Button.ƒ
+    { Button.defaults with
+        State = Button.State.Disabled }
+    [R.str "Disabled"]
+
+(* Active Button *)
+Button.ƒ
+    { Button.defaults with
+        State = Button.State.Active }
+    [R.str "Active"]
+
+(* Loading Button *)
+Button.ƒ
+    { Button.defaults with
+        State = Button.State.Loading }
+    [R.str "----"]
+"""
+
+let buttonGroupSnippet = """(* Button Group *)
+ButtonGroup.ƒ ButtonGroup.defaults [
+    Button.ƒ
+        Button.defaults
+        [R.str "First"]
+
+    Button.ƒ
+        Button.defaults
+        [R.str "Second"]
+
+    Button.ƒ
+        Button.defaults
+        [R.str "Third"]
+]
+"""
 
 let view () =
     R.div [ClassName "container"; Id "buttons"] [
@@ -26,77 +124,134 @@ let view () =
                     [R.str "Link"]
             ]
         ]
-        R.div [] [
-            R.h4 [] [R.str "Sizes"]
-            Button.ƒ
-                { Button.defaults with
-                    Size = Button.Size.Small }    
-                [R.str "Small"]
-            R.RawText "\n"
-            Button.ƒ Button.defaults [R.str "Default"]
-            R.RawText "\n"
-            Button.ƒ
-                { Button.defaults with
-                    Size = Button.Size.Large }
-                [R.str "Large"]
+        R.div [ClassName "columns"] [
+            R.div [ClassName "column col-12"] [
+                Code.ƒ
+                    { Code.defaults with 
+                        Code = Highlight.fsharp buttonKindSnippet
+                    }
+            ]
         ]
-        R.div [] [
-            R.h4 [] [R.str "Colors"]
-            Button.ƒ
-                { Button.defaults with
-                    Color = Button.Color.Success }
-                [R.str "Success"]
-            R.RawText "\n"
-            Button.ƒ
-                { Button.defaults with
-                    Kind = Button.Kind.Primary }
-                [R.str "Default"]
-            R.RawText "\n"
-            Button.ƒ
-                { Button.defaults with
-                    Color = Button.Color.Error }
-                [R.str "Error"]
-        ]
-        R.div [] [
-            R.h4 [] [R.str "Formats"]
-            Button.ƒ
-                { Button.defaults with
-                    Format = Button.Format.SquaredAction
-                    Kind = Button.Kind.Primary }
-                [ Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Plus } [] ]
-            R.RawText "\n"
-            Button.ƒ
-                { Button.defaults with
-                    Format = Button.Format.RoundAction }
-                [ Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Plus } [] ]
-        ]
-        R.div [] [
-            R.h4 [] [R.str "States"]
-            Button.ƒ
-                { Button.defaults with
-                    State = Button.State.Disabled }
-                [R.str "Disabled"]
-            R.RawText "\n"
-            Button.ƒ
-                { Button.defaults with
-                    State = Button.State.Active }
-                [R.str "Active"]
-            R.RawText "\n"
-            Button.ƒ
-                { Button.defaults with
-                    State = Button.State.Loading }
-                [R.str "Cool"]
-        ]
-        R.div [] [
-            R.h4 [] [R.str "Group"]
-            ButtonGroup.ƒ ButtonGroup.defaults [
+        R.h4 [] [R.str "Sizes"]
+        R.div [ClassName "docs-demo columns"] [
+            R.div [ClassName "column col-12"] [
                 Button.ƒ
-                    Button.defaults
-                    [R.str "First"]
+                    { Button.defaults with
+                        Size = Button.Size.Small }    
+                    [R.str "Small"]
+                R.RawText "\n"
+                Button.ƒ Button.defaults [R.str "Default"]
                 R.RawText "\n"
                 Button.ƒ
-                    Button.defaults
-                    [R.str "Second"]
+                    { Button.defaults with
+                        Size = Button.Size.Large }
+                    [R.str "Large"]
+            ]
+        ]
+        R.div [ClassName "columns"] [
+            R.div [ClassName "column col-12"] [
+                Code.ƒ
+                    { Code.defaults with 
+                        Code = Highlight.fsharp buttonSizeSnippet
+                    }
+            ]
+        ]
+        R.h4 [] [R.str "Colors"]
+        R.div [ClassName "docs-demo columns"] [
+            R.div [ClassName "column col-12"] [
+                Button.ƒ
+                    { Button.defaults with
+                        Color = Button.Color.Success }
+                    [R.str "Success"]
+                R.RawText "\n"
+                Button.ƒ
+                    { Button.defaults with
+                        Color = Button.Color.Error }
+                    [R.str "Error"]
+            ]
+        ]
+        R.div [ClassName "columns"] [
+            R.div [ClassName "column col-12"] [
+                Code.ƒ
+                    { Code.defaults with 
+                        Code = Highlight.fsharp buttonColorSnippet
+                    }
+            ]
+        ]
+        R.h4 [] [R.str "Formats"]
+        R.div [ClassName "docs-demo columns"] [
+            R.div [ClassName "column col-12"] [
+                Button.ƒ
+                    { Button.defaults with
+                        Format = Button.Format.SquaredAction
+                        Kind = Button.Kind.Primary }
+                    [ Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Plus } [] ]
+                R.RawText "\n"
+                Button.ƒ
+                    { Button.defaults with
+                        Format = Button.Format.RoundAction }
+                    [ Icon.ƒ { Icon.defaults with Kind = Icon.Kind.Plus } [] ]
+            ]
+        ]
+        R.div [ClassName "columns"] [
+            R.div [ClassName "column col-12"] [
+                Code.ƒ
+                    { Code.defaults with 
+                        Code = Highlight.fsharp buttonFormatSnippet
+                    }
+            ]
+        ]
+        R.h4 [] [R.str "States"]
+        R.div [ClassName "docs-demo columns"] [
+            R.div [ClassName "column col-12"] [
+                Button.ƒ
+                    { Button.defaults with
+                        State = Button.State.Disabled }
+                    [R.str "Disabled"]
+                R.RawText "\n"
+                Button.ƒ
+                    { Button.defaults with
+                        State = Button.State.Active }
+                    [R.str "Active"]
+                R.RawText "\n"
+                Button.ƒ
+                    { Button.defaults with
+                        State = Button.State.Loading }
+                    [R.str "Cool"]
+            ]
+        ]
+        R.div [ClassName "columns"] [
+            R.div [ClassName "column col-12"] [
+                Code.ƒ
+                    { Code.defaults with 
+                        Code = Highlight.fsharp buttonStateSnippet
+                    }
+            ]
+        ]
+        R.h4 [] [R.str "Group"]
+        R.div [ClassName "docs-demo columns"] [
+            R.div [ClassName "column col-12"] [
+                ButtonGroup.ƒ ButtonGroup.defaults [
+                    Button.ƒ
+                        Button.defaults
+                        [R.str "First"]
+                    R.RawText "\n"
+                    Button.ƒ
+                        Button.defaults
+                        [R.str "Second"]
+                    R.RawText "\n"
+                    Button.ƒ
+                        Button.defaults
+                        [R.str "Third"]
+                ]
+            ]
+        ]
+        R.div [ClassName "columns"] [
+            R.div [ClassName "column col-12"] [
+                Code.ƒ
+                    { Code.defaults with 
+                        Code = Highlight.fsharp buttonGroupSnippet
+                    }
             ]
         ]
     ]
