676a29d11539d3a7a969d1ba8a42597dae270f6f
Thomas Monte <tmonte@users.noreply.github.com>
2018-08-11 01:19:54 -0600

    Migration to Fable 2 beta (#33)
    * migrated to fable 2

--- fake.cmd
+++ fake.cmd
@@ -1,7 +1,2 @@
-SET TOOL_PATH=.fake
-
-IF NOT EXIST "%TOOL_PATH%\fake.exe" (
-  dotnet tool install fake-cli --tool-path ./%TOOL_PATH% --version 5.*
-)
-
-"%TOOL_PATH%/fake.exe" %*
+dotnet restore build.proj
+dotnet fake %*
