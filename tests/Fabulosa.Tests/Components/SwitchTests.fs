8ef7f1594305ea4377bdad1dd2a86d35576d42fe
Thomas Monte <thomasmont3@gmail.com>
2018-08-19 22:36:31 -0600

    added radio, select, switch, and textarea tests

---
+++ tests/Fabulosa.Tests/Components/SwitchTests.fs
@@ -0,0 +1,36 @@
+﻿module SwitchTests
+
+open Expecto
+open Fabulosa
+module R = Fable.Helpers.React
+open R.Props
+open Fabulosa.Tests.Extensions.TestNodeExtensions
+
+[<Tests>]
+let tests =
+    testList "Switch tests" [
+
+        test "Switch default" {
+            let props = Switch.defaults
+            let switch = Switch.ƒ props
+            switch |> hasClasses ["form-switch"]
+            switch |> hasDescendent (R.input [Type "checkbox"])
+            switch |> hasDescendent (R.i [ClassName "form-icon"] [])
+            switch |> hasDescendent (R.RawText "Label")
+        }
+
+        test "Switch text" {
+            let props = { Switch.defaults with Text = "custom" }
+            let switch = Switch.ƒ props
+            switch |> hasClasses ["form-switch"]
+            switch |> hasDescendent (R.RawText "custom")
+        }
+
+        test "Switch html props" {
+            let props = { Switch.defaults with HTMLProps = [ClassName "custom"] }
+            let switch = Switch.ƒ props
+            switch |> hasClasses ["form-switch"]
+            switch |> hasDescendent (R.input [ClassName "custom"])
+        }
+
+    ]
