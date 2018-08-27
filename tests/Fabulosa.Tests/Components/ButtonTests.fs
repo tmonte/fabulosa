1869eb73ebb32cc3e2266fe8819983c3ef058575
Leandro Miranda <mirandaleandro@gmail.com>
2018-08-27 09:05:39 -0600

    Fix Button tests according to new patterns

--- tests/Fabulosa.Tests/Components/ButtonTests.fs
+++ tests/Fabulosa.Tests/Components/ButtonTests.fs
@@ -3,126 +3,144 @@ module ButtonTests
 open Expecto
 open Fabulosa
 module R = Fable.Helpers.React
+open Fabulosa.Accordion
 open R.Props
-open Fabulosa.Tests.Extensions.TestNodeExtensions
+open Fabulosa.Tests.Extensions
 open Fabulosa.Tests.Extensions.ReactNode
+open Fabulosa.Tests.Extensions.TestNodeExtensions
 
 [<Tests>]
 let tests =
     testList "Button tests" [
 
         test "Button default" {
-            let props = Button.defaults
-            let child = R.div [ClassName "custom"] [R.str "text"]
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"]
-            button |> found child
+            let childElement = R.div [ClassName "custom"] [R.str "text"]
+            let buttonElement = Button.ƒ Button.defaults [childElement]
+            
+            buttonElement
+            |> Expect.hasUniqueClassBind "btn"
+            |> Expect.containsChild 1 childElement
         }
 
         test "Button kind primary" {
-            let props = { Button.defaults with Kind = Button.Kind.Primary }
-            let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-primary"]
-            button |> hasDescendent child
+            let childElement = R.str "text"
+            let buttonElement = Button.ƒ { Button.defaults with Kind = Button.Kind.Primary } [childElement]
+            
+            buttonElement 
+            |> Expect.containsClassNameBind "btn btn-primary"
+            |> Expect.containsChild 1 childElement
         }
 
         test "Button kind link" {
-            let props = { Button.defaults with Kind = Button.Kind.Link }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-link"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with Kind = Button.Kind.Link } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-link"
+            |> Expect.containsChild 1 child
         }
 
         test "Button color success" {
-            let props = { Button.defaults with Color = Button.Color.Success }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-success"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with Color = Button.Color.Success } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-success"
+            |> Expect.containsChild 1 child
         }
 
         test "Button color error" {
-            let props = { Button.defaults with Color = Button.Color.Error }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-error"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with Color = Button.Color.Error } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-error"
+            |> Expect.containsChild 1 child
         }
 
         test "Button size small" {
-            let props = { Button.defaults with Size = Button.Size.Small }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-sm"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with Size = Button.Size.Small } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-sm"
+            |> Expect.containsChild 1 child
         }
 
         test "Button size large" {
-            let props = { Button.defaults with Size = Button.Size.Large }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-lg"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with Size = Button.Size.Large } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-lg"
+            |> Expect.containsChild 1 child
         }
 
         test "Button state disabled" {
-            let props = { Button.defaults with State = Button.State.Disabled }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "disabled"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with State = Button.State.Disabled  } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn disabled"
+            |> Expect.containsChild 1 child
         }
 
         test "Button state active" {
-            let props = { Button.defaults with State = Button.State.Active }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "active"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with State = Button.State.Active } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn active"
+            |> Expect.containsChild 1 child
         }
 
         test "Button state loading" {
-            let props = { Button.defaults with State = Button.State.Loading }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "loading"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with State = Button.State.Loading } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn loading"
+            |> Expect.containsChild 1 child
         }
 
         test "Button format squared action" {
-            let props = { Button.defaults with Format = Button.Format.SquaredAction }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-action"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with Format = Button.Format.SquaredAction } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-action"
+            |> Expect.containsChild 1 child
         }
 
         test "Button format round action" {
-            let props = { Button.defaults with Format = Button.Format.RoundAction }
             let child = R.str "text"
-            let button = Button.ƒ props [child]
-            button |> hasClasses ["btn"; "btn-action"; "circle"]
-            button |> hasDescendent child
+            let buttonElement = Button.ƒ { Button.defaults with Format = Button.Format.RoundAction } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-action circle"
+            |> Expect.containsChild 1 child
         }
 
         test "Button children with name" {
-            let props = Button.defaults
             let grandChild = R.span [] []
             let child = R.div [] [grandChild]
-            let button = Button.ƒ props [child]
-            button |> hasDescendent child
-            button |> hasDescendent grandChild
+            let buttonElement = Button.ƒ Button.defaults [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn"
+            |> Expect.containsChildBind 1 child
+            |> Expect.containsChild 1 grandChild
         }
 
         test "Button children with class" {
-            let props = Button.defaults
             let grandChild = R.span [ClassName "grand-child"] []
             let child = R.div [ClassName "child"] [grandChild]
-            let button = Button.ƒ props [child]
-            button |> hasDescendent child
-            button |> hasDescendent grandChild
+            let buttonElement = Button.ƒ { Button.defaults with Size = Button.Size.Small } [child]
+
+            buttonElement
+            |> Expect.containsClassNameBind "btn btn-sm"
+            |> Expect.containsChildBind 1 child
+            |> Expect.containsChild 1 grandChild
         }
 
     ]
