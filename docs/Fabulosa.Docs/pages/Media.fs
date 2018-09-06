ad7012ba94e43e9c64c26a240b6dd8a227541291
Leandro Miranda <mirandaleandro@gmail.com>
2018-09-06 08:16:28 -0600

    Add Caption to docs Add Figure documentation on to docs

--- docs/Fabulosa.Docs/pages/Media.fs
+++ docs/Fabulosa.Docs/pages/Media.fs
@@ -53,8 +53,7 @@ let figure =
         }  
         Caption = { Media.Caption.defaults with Text = [R.str "Ciro Gomes Presidente 2018"]}    
     }
-     
-
+    
 (*** hide ***)
 let render () =
     tryMount "media-img-responsive-demo-a" (image "5rem")
@@ -63,7 +62,9 @@ let render () =
     tryMount "media-img-fit-contain-demo" imageContain
     tryMount "media-img-fit-cover-demo" imageCover
     tryMount "media-img-props-table" (PropTable.propTable typeof<Media.Image.Props> Media.Image.defaults)
+    tryMount "figure-props-table" (PropTable.propTable typeof<Media.Figure.Props> Media.Figure.defaults)
     tryMount "figure-demo" figure
+    tryMount "caption-props-table" (PropTable.propTable typeof<Media.Caption.Props> Media.Caption.defaults)
 (**
 <div id="media">
     <h2 class="s-title">
@@ -147,14 +148,35 @@ Add the ```img-responsive``` class to <img> elements. The images will scale with
 
 (**
 
-<div id="images">
+<div id="figure">
     <h3 class="s-title">
-        Figure
+        Figure/Caption
     </h3>
 </div>    
     
-Serves as a container for reponsive images and caption.
+Figure serves as a container for reponsive images and caption. Caption defines the text(or any ReactElement) that is displayed as metadata about the image.
 
+<div id="figure-props">
+    <h4 class="s-title">
+        Figure Props
+    </h4>
+</div>    
+
+<div class="props-table" id="figure-props-table"></div>
+
+<div id="caption-props">
+    <h4 class="s-title">
+        Caption Props
+    </h4>
+</div>    
+
+<div class="props-table" id="caption-props-table"></div>
+
+<div id="figure">
+    <h4 class="s-title">
+        Demo
+    </h4>
+</div>    
 <div class="demo">
     <div class="columns">
         <div class="column col-6 col-mx-auto">
