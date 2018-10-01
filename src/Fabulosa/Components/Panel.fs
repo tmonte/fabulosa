d2593b906aecd9145b6cd4ff31ad5a7347c82b91
Thomas Monte <thomasmont3@gmail.com>
2018-09-30 18:19:17 -0600

    panels, missing docs

---
+++ src/Fabulosa/Components/Panel.fs
@@ -0,0 +1,91 @@
+﻿namespace Fabulosa
+
+[<RequireQualifiedAccess>]
+module Panel =
+
+    open Fabulosa.Extensions
+    module R = Fable.Helpers.React
+    open R.Props
+    open Fable.Import.React
+
+    [<RequireQualifiedAccess>]
+    module Header =
+
+        [<RequireQualifiedAccess>]
+        type T = ReactElement list
+
+        let ƒ (header: T) =
+            R.div
+                [ ClassName "panel-header" ]
+                header
+
+    [<RequireQualifiedAccess>]
+    module Nav =
+
+        [<RequireQualifiedAccess>]
+        type T = ReactElement list
+
+        let ƒ (nav: T) =
+            R.div
+                [ ClassName "panel-nav" ]
+                nav
+
+    [<RequireQualifiedAccess>]
+    module Body =
+
+        [<RequireQualifiedAccess>]
+        type T = ReactElement list
+
+        let ƒ (body: T) =
+            R.div
+                [ ClassName "panel-body" ]
+                body
+
+    [<RequireQualifiedAccess>]
+    module Footer =
+
+        [<RequireQualifiedAccess>]
+        type T = ReactElement list
+
+        let ƒ (footer: T) =
+            R.div
+                [ ClassName "panel-footer" ]
+                footer
+
+    [<RequireQualifiedAccess>]
+    type Props =
+        { HTMLProps: HTMLProps }
+
+    [<RequireQualifiedAccess>]
+    type Children =
+        { Header: (ReactElement list) option
+          Nav: (ReactElement list) option
+          Body: (ReactElement list) option
+          Footer: (ReactElement list) option }
+
+    [<RequireQualifiedAccess>]
+    type T = Props * Children
+
+    let props =
+        { Props.HTMLProps = [] }
+
+    let children =
+        { Children.Header = None
+          Children.Nav = None
+          Children.Body = None
+          Children.Footer = None }
+
+    let private child kind =
+        function
+        | Some elements -> R.div [ ClassName ("panel-" + kind) ] elements
+        | None -> R.ofOption None
+
+    let ƒ (panel: T) =
+        let props, children = panel
+        props.HTMLProps
+        |> addProp (ClassName "panel")
+        |> R.div
+        <| [ child "header" children.Header
+             child "nav" children.Nav
+             child "body" children.Body
+             child "footer" children.Footer ]
---
+++ tests/Fabulosa.Tests/Components/PanelTests.fs
@@ -0,0 +1,71 @@
+﻿module PanelTests
+
+open Expecto
+open Fabulosa
+module R = Fable.Helpers.React
+open R.Props
+open Expect
+
+
+[<Tests>]
+let tests =
+    testList "Panel tests" [
+
+        test "Panel default" {
+           Panel.ƒ
+               (Panel.props, Panel.children)
+            |> ReactNode.unit
+            |> hasUniqueClass "panel"
+        }
+
+        test "Panel html props" {
+            Panel.ƒ
+                ({ Panel.props with
+                     HTMLProps =
+                       [ ClassName "custom" ] },
+                 Panel.children)
+            |> ReactNode.unit
+            |> hasClass "custom"
+        }
+
+        test "Panel header" {
+            let header = [ R.str "Header" ]
+            Panel.ƒ
+                (Panel.props,
+                 { Panel.children with
+                     Header = Some header })
+            |> ReactNode.unit
+            |> hasChild 1 (Panel.Header.ƒ header |> ReactNode.unit)
+        }
+
+        test "Panel nav" {
+            let nav = [ R.str "Nav" ]
+            Panel.ƒ
+                (Panel.props,
+                 { Panel.children with
+                     Nav = Some nav })
+            |> ReactNode.unit
+            |> hasChild 1 (Panel.Nav.ƒ nav |> ReactNode.unit)
+        }
+
+        test "Panel body" {
+            let body = [ R.str "Body" ]
+            Panel.ƒ
+                (Panel.props,
+                 { Panel.children with
+                     Body = Some body })
+            |> ReactNode.unit
+            |> hasChild 1 (Panel.Body.ƒ body |> ReactNode.unit)
+        }
+
+        test "Panel footer" {
+            let footer = [ R.str "Footer" ]
+            Panel.ƒ
+                (Panel.props,
+                 { Panel.children with
+                     Footer = Some footer })
+            |> ReactNode.unit
+            |> hasChild 1 (Panel.Footer.ƒ footer |> ReactNode.unit)
+        }
+
+    ]
