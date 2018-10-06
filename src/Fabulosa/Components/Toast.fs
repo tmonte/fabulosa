c8776c4a5d93a3ac5be0ed6bc067b35b1eba57ff
Thomas Monte <thomasmont3@gmail.com>
2018-10-06 09:55:03 -0600

    toast wip

---
+++ src/Fabulosa/Components/Toast.fs
@@ -0,0 +1,28 @@
+﻿namespace Fabulosa
+
+[<RequireQualifiedAccess>]
+module Toast =
+
+    open Fabulosa.Extensions
+    module R = Fable.Helpers.React
+    open R.Props
+    open Fable.Import.React
+
+    [<RequireQualifiedAccess>]
+    type Props =
+        { HTMLProps: HTMLProps }
+
+    [<RequireQualifiedAccess>]
+    type Children = string
+
+    [<RequireQualifiedAccess>]
+    type T = Props * Children
+
+    let props =
+        { Props.HTMLProps = [] }
+
+    let ƒ (toast: T) =
+        let props, children = toast
+        props.HTMLProps
+        |> addProp (ClassName "toast")
+        |> R.div <| [ R.str children ]
---
+++ tests/Fabulosa.Tests/Components/ToastTests.fs
@@ -0,0 +1,42 @@
+﻿module ToastTests
+
+open Expecto
+open Fabulosa
+module R = Fable.Helpers.React
+open R.Props
+open Expect
+
+
+[<Tests>]
+let tests =
+    testList "Toast tests" [
+
+        test "Toast default" {
+           Toast.ƒ
+               (Toast.props, "Toast")
+            |> ReactNode.unit
+            |> hasUniqueClass "toast"
+        }
+
+        test "Toast html props" {
+            Toast.ƒ
+                ({ Toast.props with
+                     HTMLProps =
+                       [ ClassName "custom" ] },
+                 "")
+            |> ReactNode.unit
+            |> hasClass "custom"
+        }
+
+        test "Toast children" {
+            Toast.ƒ
+                (Toast.props, "Toast")
+            |> ReactNode.unit
+            |> hasText "Toast"
+        }
+
+        // colors
+
+        // onRequestClose
+       
+    ]
