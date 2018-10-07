a765a7e2a15f76dfc124a8b035060bf9b2a6cb41
Thomas Monte <thomasmont3@gmail.com>
2018-10-06 23:35:44 -0600

    toast done, closes #67

--- docs/Fabulosa.Docs/pages/Toast.fs
+++ docs/Fabulosa.Docs/pages/Toast.fs
@@ -1,3 +1,153 @@
 ﻿module ToastPage
 
-let render () = ()
+open Fabulosa
+open Fabulosa.Docs
+module R = Fable.Helpers.React
+open R.Props
+open Fable.Import.React
+open Renderer
+open Microsoft.FSharp.Core
+open Fable.Import.JS
+open Fable.Helpers.React.ReactiveComponents
+
+type State =
+    { Opened: bool }
+
+type Message = 
+    | Toggle
+
+type Dispatch = Message -> unit
+
+let init _ =
+    { Opened = false }
+
+let update message state =
+    match message with 
+    | Toggle ->
+        { state with Opened = not state.Opened }
+
+let toggler dispatch opened =
+    Button.ƒ
+        ({ Button.props  with
+             Kind = Button.Kind.Primary
+             HTMLProps =
+               [ OnClick
+                   (fun _ ->
+                      dispatch Toggle
+                      Fable.Import.JS.setTimeout
+                        (fun () -> dispatch Toggle)
+                        3000 |> ignore)
+                 Disabled opened ] },
+         [ R.str "Open" ])
+
+let trigger dispatch opened =
+    Button.ƒ
+        ({ Button.props  with
+             Kind = Button.Kind.Primary
+             HTMLProps =
+               [ OnClick
+                   (fun _ -> dispatch Toggle)
+                 Disabled opened ] },
+         [ R.str "Open" ])
+
+(*** define: toast-default-sample ***)
+let toast =
+    Toast.ƒ
+        (Toast.props, "Default toast")
+(*** define: toast-primary-sample ***)
+let primary dispatch =
+    Toast.ƒ
+        ({ Toast.props with
+             Color = Toast.Color.Primary
+             OnRequestClose = Some (fun _ -> dispatch Toggle )},
+         "You can close me!")
+(*** hide ***)
+
+let defaultView (model: Model<unit, State>) dispatch =
+    let renderToast =
+        if model.state.Opened then
+            toast
+        else R.ofOption None
+    R.div [] 
+        [ toggler dispatch model.state.Opened
+          renderToast ]
+
+let primaryView (model: Model<unit, State>) dispatch =
+    let renderToast =
+        if model.state.Opened then
+            primary dispatch
+        else R.ofOption None
+    R.div [] 
+        [ trigger dispatch model.state.Opened
+          renderToast ]
+
+let demo view  =
+    R.reactiveCom init update view "" () []
+
+(*** hide ***)
+let render () =
+    tryMount "toast-default-demo" <| demo defaultView
+    tryMount "toast-primary-demo" <| demo primaryView
+    tryMount "toast-props-table"
+        (PropTable.propTable typeof<Toast.Props> Toast.props)
+(**
+
+<div id="toast">
+
+<h2 class="s-title">
+    Toast
+</h2>
+
+Toasts are used to show alert
+or information to users
+
+</div>
+
+<div id="toast-props">
+
+<h3 class="s-title">
+    Props
+</h3>
+
+<div class="props-table" id="toast-props-table"></div>
+
+</div>
+
+<div id="toast-default">
+
+<h3 class="s-title">
+    Default
+</h3>
+
+A simple toast component with all defaults.
+
+<div class="demo" id="toast-default-demo"></div>
+
+*)
+
+(*** include: toast-default-sample ***)
+
+(**
+
+</div>
+
+<div id="toast-primary">
+
+<h3 class="s-title">
+    Primary & Closeable
+</h3>
+
+A toast with primary color
+that you can close
+
+<div class="demo" id="toast-primary-demo"></div>
+
+*)
+
+(*** include: toast-primary-sample ***)
+
+(**
+
+</div>
+
+*)
