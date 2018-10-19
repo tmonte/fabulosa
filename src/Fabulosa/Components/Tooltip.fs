17e11c9c4eacb1d27692e1d777d2d875824cd6dd
Leandro Miranda <mirandaleandro@gmail.com>
2018-10-19 13:17:22 -0600

    WIP Tooltip

--- src/Fabulosa/Components/Tooltip.fs
+++ src/Fabulosa/Components/Tooltip.fs
@@ -1,65 +1,12 @@
 namespace Fabulosa
 open Fable.Import.React
 
-module HoverContainer =
-    module R = Fable.Helpers.React
-    open R.Props
-    open Fable.Import.React
-    open Fabulosa.Extensions
-    open Extensions.Fable.Helpers.React.Props
-    open Fable.Import
-    open R.ReactiveComponents
-
-    type State =
-       | Hovering of Browser.Element 
-       | NotHovering
-    
-    [<RequireQualifiedAccess>]
-    type Props = State -> ReactElement
- 
-    [<RequireQualifiedAccess>]
-    type T = Props
-    
-    type Messages = 
-    | MouseEnter of Browser.Element
-    | MouseLeave
-          
-    let onMouseEnter dispatch (event: MouseEvent) =
-        event.currentTarget 
-        :?> Browser.Element
-        |> MouseEnter
-        |> dispatch 
-    let onMouseLeave dispatch (event: MouseEvent) = dispatch MouseLeave 
-          
-    let props: Props = (fun _ -> R.fragment [] [])
-           
-    let private init (props: Props) = NotHovering
-
-    let private update message state =
-        match message with
-        | MouseEnter element -> Hovering element
-        | MouseLeave _ -> NotHovering
-    
-    let private view (model: Model<Props, State>) dispatch = 
-            R.span [ ClassName "hover-container"; OnMouseEnter (onMouseEnter dispatch)(*; OnMouseLeave (onMouseLeave dispatch)*) ] [ (model.props model.state) ]
-     
-    let ƒ (hover: T) =
-        R.reactiveCom
-            init
-            update
-            view
-            ""
-            hover
-            []
-
 open Fable.Import
 module R = Fable.Helpers.React
 open R.Props
 open Extensions.Fable.Helpers.React.Props
 open Fable.Core
 open Fable.Helpers
-open Fable.Import
-open Fable.Import
 
 module Tooltip =
     module R = Fable.Helpers.React
@@ -86,7 +33,18 @@ module Tooltip =
     type Children = ReactElement
     
     [<RequireQualifiedAccess>]
-    type T = Props * Children       
+    type T = Props * Children 
+        
+    type TooltipRequired = 
+        Content of ReactElement list
+    
+    type TooltipChildren = Target of ReactElement list
+    
+    type TooltipOptional =
+        | Orientation of Orientation
+        interface IHTMLProp
+    
+    type Tooltip = HTMLProps * TooltipRequired * TooltipChildren       
     
     let props =
         { Props.HTMLProps = []
@@ -155,37 +113,37 @@ module Tooltip =
         | Hovering
         | NotHovering   
                  
-        type State = {
-            Style: HTMLAttr
-            Hover: Hover
-        }
+        type State = { Style: HTMLAttr }
                
         type HoverClass(refCallBack) =
             inherit PureComponent<Props, State>(refCallBack)
-            do base.setInitState 
-                { Style =  Style [Top (sprintf "%dpx" -9000); Left (sprintf "%dpx" -9000); Position "fixed"]
-                  Hover = NotHovering }
+            do base.setInitState { Style =  Style [] }
                   
             let mutable targetElement = None
             let mutable tooltipElement = None  
             
-            let bottomPosition (target: BoundingRect) (tooltip: BoundingRect) =
+            let display =
+                function 
+                | Hovering -> Opacity "1"
+                | NotHovering -> Opacity "0" 
+    
+            let bottomPosition (target: BoundingRect) (tooltip: BoundingRect) hover =
                 let horizontalCenter = target.Left + (target.Width / 2) - (tooltip.Width / 2)
-                Style [Top (sprintf "%dpx" (target.Bottom));Left (sprintf "%dpx" horizontalCenter); Position "fixed"]
+                Style [Top (sprintf "%dpx" (target.Bottom));Left (sprintf "%dpx" horizontalCenter); Position "fixed"; display hover]
             
-            let topPosition (target: BoundingRect) (tooltip: BoundingRect) =
+            let topPosition (target: BoundingRect) (tooltip: BoundingRect) hover =
                 let horizontalCenter = target.Left + (target.Width / 2) - (tooltip.Width / 2)
-                Style [Top (sprintf "%dpx" (target.Top - tooltip.Height));Left (sprintf "%dpx" horizontalCenter); Position "fixed"]
+                Style [Top (sprintf "%dpx" (target.Top - tooltip.Height));Left (sprintf "%dpx" horizontalCenter); Position "fixed"; display hover]
                 
-            let leftPosition (target: BoundingRect) (tooltip: BoundingRect) =
+            let leftPosition (target: BoundingRect) (tooltip: BoundingRect) hover =
                 let verticalCenter = target.Top + (target.Height / 2) - (tooltip.Height / 2)
-                Style [Top (sprintf "%dpx" verticalCenter);Left (sprintf "%dpx" (target.Left - tooltip.Width)); Position "fixed"]
+                Style [Top (sprintf "%dpx" verticalCenter);Left (sprintf "%dpx" (target.Left - tooltip.Width)); Position "fixed"; display hover]
             
-            let rightPosition (target: BoundingRect) (tooltip: BoundingRect) =
+            let rightPosition (target: BoundingRect) (tooltip: BoundingRect) hover =
                 let verticalCenter = target.Top + (target.Height / 2) - (tooltip.Height / 2)
-                Style [Top (sprintf "%dpx" verticalCenter);Left (sprintf "%dpx" (target.Left + target.Width)); Position "fixed"]
-                        
-            let calculatePosition (targetElement:Browser.Element) (tooltipElement:Browser.Element) orientation =
+                Style [Top (sprintf "%dpx" verticalCenter);Left (sprintf "%dpx" (target.Left + target.Width)); Position "fixed"; display hover]
+                                        
+            let calculatePosition (targetElement:Browser.Element) (tooltipElement:Browser.Element) orientation hover =
                 let targetBoundaries = targetElement.getBoundingClientRect() |> BoundingRect.OfClientRectType
                 let tooltipBoundaries = tooltipElement.getBoundingClientRect() |> BoundingRect.OfClientRectType
                 match orientation with 
@@ -193,7 +151,7 @@ module Tooltip =
                  | Orientation.Right -> rightPosition
                  | Orientation.Bottom -> bottomPosition
                  | Orientation.Left -> leftPosition
-                <|| (targetBoundaries, tooltipBoundaries)
+                <||| (targetBoundaries, tooltipBoundaries, hover)
                 
             let setTooltipRef (ref:Browser.Element) = 
                 if ref <> null then
@@ -202,38 +160,62 @@ module Tooltip =
             let setTargetRef (ref:Browser.Element) = 
                 if ref <> null then
                     targetElement <- Some ref
+                        
+            override this.componentDidMount () = this.updatePosition NotHovering
             
-            override this.componentDidMount () = this.updatePosition ()
-            member this.onMouseEnter (event: MouseEvent) = this.updatePosition ()
-            member this.onMouseLeave (event: MouseEvent) = this.updatePosition ()
-            member this.updateStyle style = this.setState { Style = style; Hover = this.state.Hover}
+            member this.onMouseEnter (event: MouseEvent) =
+                this.updatePosition Hovering
                 
-            member this.updatePosition () =
+            member this.onMouseLeave (event: MouseEvent) =
+                this.updatePosition NotHovering
+                
+            member this.updatePosition hover =
                 let mapElements target tooltip = 
-                    let style = calculatePosition target tooltip this.props.Orientation
-                    this.updateStyle style
+                    let style = calculatePosition target tooltip this.props.Orientation hover
+                    this.setState { Style = style;}
                     
                 Option.map2 mapElements targetElement tooltipElement
                 |> ignore 
-             
+                
+            member this.basetooltipProps () =
+                  props.HTMLProps
+                  |> addProp this.state.Style    
+            
             override this.render() =
                 let updatedProps =
                     props.HTMLProps
                     |> addProp this.state.Style
             
                 R.fragment [] 
-                    [ R.span [Ref setTargetRef; OnMouseEnter this.onMouseEnter; OnMouseLeave this.onMouseLeave] 
-                        [ R.str "Mein kinder von himmel"]
-                      BaseTooltip.ƒ ( { Ref = setTooltipRef; HTMLProps = updatedProps; Orientation = props.Orientation}, this.props.TooltipContent)
-                      |> Portal.ƒ "tooltip-yo" ]
-                        
-        let hoverTooltip props = React.ofType<HoverClass, Props, _> props []
-            
-        let build baseTooltipƒ (tooltip: T) =
-            let props, children = tooltip
-            hoverTooltip { props with TooltipContent = props.TooltipContent; Orientation = props.Orientation }
+                    [ R.span [Ref setTargetRef; OnMouseEnter this.onMouseEnter; OnMouseLeave this.onMouseLeave] this.children
+                      BaseTooltip.ƒ ( { Ref = setTooltipRef; HTMLProps = this.basetooltipProps(); Orientation = props.Orientation}, this.props.TooltipContent)
+                      |> Portal.ƒ "tooltip-portal"]
 
-        let ƒ tooltip = build BaseTooltip.ƒ tooltip
+    let private pick fn (props:HTMLProps) =
+            props |> List.tryPick fn
+
+    let private orientation =
+        fun (prop: IHTMLProp) ->
+            match prop with
+            | :? TooltipOptional as opt ->
+                match opt with
+                | Orientation ori ->
+                    Some (ori)
+                | _ -> None
+            | _ -> None
+        |> pick
+            
+    let tooltip (tooltip: Tooltip) =
+        let opt, TooltipRequired.Content content, TooltipChildren.Target children = tooltip
         
-        
-    
+        let tooltipOrientation =
+            match orientation opt with 
+            | Some s -> s
+            | None -> Orientation.Top
+                                    
+        React.ofType<Hover.HoverClass, Props, _> 
+            { HTMLProps = opt
+              Orientation = tooltipOrientation
+              TooltipContent = TooltipContent.Elements content }
+            children
+      
