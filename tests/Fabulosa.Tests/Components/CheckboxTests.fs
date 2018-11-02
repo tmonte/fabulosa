306b16c1d950b4d96499cf45544db831f39ca0f6
Thomas Monte <thomasmonte@Thomass-MacBook-Pro.local>
2018-11-02 15:56:26 -0600

    checkbox radio and switch tests

--- tests/Fabulosa.Tests/Components/CheckboxTests.fs
+++ tests/Fabulosa.Tests/Components/CheckboxTests.fs
@@ -2,6 +2,7 @@
 
 open Expecto
 open Fabulosa
+open Fabulosa.Checkbox
 module R = Fable.Helpers.React
 open R.Props
 open Expect
@@ -10,45 +11,39 @@ open Expect
 let tests =
     testList "Checkbox tests" [
 
-        //test "Checkbox default" {
-        //    let input =
-        //        R.input [Type "checkbox"]
-        //        |> ReactNode.unit
-        //    let icon =
-        //        R.i [ClassName "form-icon"] []
-        //        |> ReactNode.unit
-        //    let label =
-        //        R.str "Checkbox"
-        //        |> ReactNode.unit
+        test "Checkbox default" {
+           let input =
+               R.input [Type "checkbox"]
+               |> ReactNode.unit
+           let icon =
+               R.i [ClassName "form-icon"] []
+               |> ReactNode.unit
+           let label =
+               R.str "Checkbox"
+               |> ReactNode.unit
 
-        //    Checkbox.ƒ
-        //        (Checkbox.props, "Checkbox")
-        //    |> ReactNode.unit
-        //    |>! hasUniqueClass "form-checkbox"
-        //    |>! hasChild 1 input
-        //    |>! hasChild 1 icon
-        //    |>! hasChild 1 label
-        //    |> hasText "Checkbox"
+           checkbox ([], Text "Checkbox")
+           |> ReactNode.unit
+           |>! hasUniqueClass "form-checkbox"
+           |>! hasChild 1 input
+           |>! hasChild 1 icon
+           |>! hasChild 1 label
+           |> hasText "Checkbox"
 
-        //}
+        }
 
-        //test "Checkbox inline" {
-        //    Checkbox.ƒ
-        //        ({ Checkbox.props with
-        //             Inline = true },
-        //         "Checkbox")
-        //    |> ReactNode.unit
-        //    |> hasClass "form-inline"
-        //}
+        test "Checkbox inline" {
+           checkbox ([ Inline ], Text "Checkbox")
+           |> ReactNode.unit
+           |> hasClass "form-inline"
+        }
 
-        //test "Checkbox html props" {
-        //    let prop = ClassName "custom"
-        //    Checkbox.ƒ
-        //        ({ Checkbox.props with
-        //             HTMLProps = [ prop ] }, "Checkbox")
-        //    |> ReactNode.unit
-        //    |>! hasUniqueClass "form-checkbox"
-        //    |> hasDescendentProp prop
-        //}
+        test "Checkbox html props" {
+           let prop = ClassName "custom"
+           checkbox ([ prop ], Text "Checkbox")
+           |> ReactNode.unit
+           |>! hasUniqueClass "form-checkbox"
+           |> hasDescendentProp prop
+        }
 
     ]
--- tests/Fabulosa.Tests/Components/RadioTests.fs
+++ tests/Fabulosa.Tests/Components/RadioTests.fs
@@ -2,6 +2,7 @@
 
 open Expecto
 open Fabulosa
+open Fabulosa.Radio
 module R = Fable.Helpers.React
 open R.Props
 open Expect
@@ -10,45 +11,38 @@ open Expect
 let tests =
     testList "Radio tests" [
 
-        //test "Radio default" {
-        //    let input =
-        //        R.input [Type "radio"]
-        //        |> ReactNode.unit
-        //    let icon =
-        //        R.i [ClassName "form-icon"] []
-        //        |> ReactNode.unit
-        //    let label =
-        //        R.str "Radio"
-        //        |> ReactNode.unit
+        test "Radio default" {
+            let input =
+                R.input [Type "radio"]
+                |> ReactNode.unit
+            let icon =
+                R.i [ClassName "form-icon"] []
+                |> ReactNode.unit
+            let label =
+                R.str "Radio"
+                |> ReactNode.unit
 
-        //    Radio.ƒ
-        //        (Radio.props, "Radio")
-        //    |> ReactNode.unit
-        //    |>! hasUniqueClass "form-radio"
-        //    |>! hasChild 1 input
-        //    |>! hasChild 1 icon
-        //    |>! hasChild 1 label
-        //    |> hasText "Radio"
+            radio ([], Text "Radio")
+            |> ReactNode.unit
+            |>! hasUniqueClass "form-radio"
+            |>! hasChild 1 input
+            |>! hasChild 1 icon
+            |>! hasChild 1 label
+            |> hasText "Radio"
+        }
 
-        //}
+        test "Radio inline" {
+            radio ([ Inline ], Text "Radio")
+            |> ReactNode.unit
+            |> hasClass "form-inline"
+        }
 
-        //test "Radio inline" {
-        //    Radio.ƒ
-        //        ({ Radio.props with
-        //             Inline = true },
-        //         "Radio")
-        //    |> ReactNode.unit
-        //    |> hasClass "form-inline"
-        //}
-
-        //test "Radio html props" {
-        //    let prop = ClassName "custom"
-        //    Radio.ƒ
-        //        ({ Radio.props with
-        //             HTMLProps = [ prop ] }, "Radio")
-        //    |> ReactNode.unit
-        //    |>! hasUniqueClass "form-radio"
-        //    |> hasDescendentProp prop
-        //}
+        test "Radio html props" {
+            let prop = ClassName "custom"
+            radio ([ prop ], Text "Radio")
+            |> ReactNode.unit
+            |>! hasUniqueClass "form-radio"
+            |> hasDescendentProp prop
+        }
 
     ]
