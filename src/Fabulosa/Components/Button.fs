88746ff36f281736cdd73c9ffa17bc4dd2555e55
Thomas Monte <thomasmonte@gmail.com>
2018-11-18 19:25:43 -0700

    separate button & group

--- src/Fabulosa/Components/Button.fs
+++ src/Fabulosa/Components/Button.fs
@@ -70,38 +70,3 @@ module Button =
     let button = createButton R.button
 
     let anchor = createButton R.a
-
-module ButtonGroup =
-
-    open Fabulosa.Extensions
-    open Button
-    module R = Fable.Helpers.React
-    open R.Props
-    
-    type ButtonGroupOptional =
-        | Block
-        interface IHTMLProp
-
-    type ButtonGroupChild =
-        Button of Button
-
-    type ButtonGroupChildren = ButtonGroupChild list
-
-    type ButtonGroup = HTMLProps * ButtonGroupChildren
-
-    let private propToClassName (prop: IHTMLProp) =
-        match prop with
-        | :? ButtonGroupOptional as opt ->
-            match opt with
-            | Block -> "btn-group-block"
-            |> ClassName
-            :> IHTMLProp
-        | _ -> prop
-
-    let buttonGroup ((opt, chi): ButtonGroup) =
-        Unmerged opt
-        |> addProp (ClassName "btn-group")
-        |> map propToClassName
-        |> merge
-        |> R.div
-        <| Seq.map (fun (Button btn) -> button btn) chi
